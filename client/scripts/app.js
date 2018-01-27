b59be6d0c00e5af62b1df89b6105554687b298ee
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-26 17:32:46 -0800

    server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,66 @@
 // YOUR CODE HERE:
+// var message = {
+//   username: username,
+//   text: text,
+//   roomname: roomname
+// };
+
+var app = {
+};
+
+//app.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
+
+app.init = function() {
+
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: message,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent', data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    //data: message,
+    //contentType: 'application/json'
+    success: function() {
+      console.log('FETCH successful', data);
+    },
+    error: function (data) {
+      console.error('FETCH failed', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+
+};
+
+app.renderMessages = function(message) {
+
+};
+
+app.renderRoom = function() {
+
+};
+
+
+
+
+
+
+
+
+
