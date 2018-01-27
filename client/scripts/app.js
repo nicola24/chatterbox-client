d9b953ffe247c5848b863a5827dd5897c7a3f91f
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-26 21:34:14 -0800

    getting there..

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,65 +1,87 @@
-// YOUR CODE HERE:
-// var message = {
-//   username: username,
-//   text: text,
-//   roomname: roomname
-// };
+$(document).ready(function() {});
 
-var app = {
-};
-
-//app.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
-
-app.init = function() {
-
-};
-
-app.send = function(message) {
-  $.ajax({
-    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
-    type: 'POST',
-    data: message,
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent', data);
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
-};
-
-app.fetch = function() {
-  $.ajax({
-    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
-    type: 'GET',
-    //data: message,
-    //contentType: 'application/json'
-    success: function() {
-      console.log('FETCH successful', data);
-    },
-    error: function (data) {
-      console.error('FETCH failed', data);
-    }
-  });
-};
-
-app.clearMessages = function() {
-
-};
-
-app.renderMessages = function(message) {
-
-};
-
-app.renderRoom = function() {
-
-};
+class App {
+  constructor() {
+    this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
+    // this.message = {
+    //   username: username, 
+    //   text: text,
+    //   roomname: roomname
+    // };
+  
+  }
 
 
+  init() {
+
+  }
+
+  send(message) {
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      data: this.message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent', data);
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+
+  fetch() {
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      data: 'order=-createAt&limit=1000', // limit at 1000 messages
+      success: function(data) {
+        console.log('FETCH successful', data);
+        // var div = document.getElementById('chats');
+        // div.innerHTML = JSON.stringify(data);
+
+        var $chat = $('#chats');
+
+        for (var i = 0; i < data.results.length; i++) { 
+          var $createAt = $('<li id="createAt">' + data.results[i].createAt + '</li>');
+          var $username = $('<button id="username">' + data.results[i].username + '</button>');
+          var $text = $('<p id="username">' + data.results[i].text + '</p>');
+          
+          $username.appendTo($chat);
+          $text.appendTo($chat);
+          $createAt.appendTo($chat);
+          
+        }
+          
+          
+
+      },
+      error: function (data) {
+        console.error('FETCH failed', data);
+      }
+    });
+
+  }
+
+  clearMessages() {
+    $('button').click(function() {
+      $('#chats').empty();
+    });  
+  }
+
+  renderMessages(message) {
+
+  }
+
+  renderRoom() {
+
+  }
 
 
+}
 
+const app = new App();
 
 
 
