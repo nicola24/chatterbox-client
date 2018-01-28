5a75e5baf5db06bebee5269234615788ccfe69ba
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-27 17:34:56 -0800

    working on chatterbox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,21 +1,18 @@
-
-
 class App {
   constructor() {
     this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
   }
 
-  // init call the fetch method
+  // *** init *** 
   init() {
-    this.fetch();
   }
 
-  // submit a POST request via $.ajax
+  // *** submit a POST request via $.ajax ***
   send(message) {
     $.ajax({
       url: this.server,
       type: 'POST',
-      data: this.message,
+      data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent', data);
@@ -26,7 +23,7 @@ class App {
     });
   }
 
-  // submit a GET request via $.ajax
+  // *** submit a GET request via $.ajax ***
   fetch() {
     $.ajax({
       url: this.server,
@@ -38,16 +35,19 @@ class App {
         // div.innerHTML = JSON.stringify(data);s
 
         var $chat = $('#chats');
-
+        
         for (var i = 0; i < data.results.length; i++) { // iterate from the end of the array, aka the most recent posts
-          
-          var $username = $('<button id="username">' + data.results[i].username + '</button>');
-          var $text = $('<li id="username">' + data.results[i].text + '</li>');
+
+          var $fetchData = $('<li id="fetchData"></li>');
+          var $username = $('<button id="username">' + _.escape(data.results[i].username) + '</button>');
+          var $text = $('<p id="username">' + _.escape(data.results[i].text) + '</p>');
           var $createdAt = $('<p id="createAt">' + data.results[i].createdAt + '</p>');
           
-          $username.appendTo($chat);
-          $text.appendTo($chat);
-          $createdAt.appendTo($chat);
+          
+          $username.appendTo($fetchData);
+          $text.appendTo($fetchData);
+          $createdAt.appendTo($fetchData);
+          $fetchData.appendTo($chat);
           
         }
       },
@@ -61,35 +61,66 @@ class App {
   // clearMessages() {
   // }
 
-  // renderMessages(message) {
-  // }
+  // *** submit a message *** 
+  renderMessages() {
+    var username = window.location.search.slice(10);
+    var $messageInput = $('#inputMessage').val();
+    
+    var message = new Message(username, $messageInput, 'lobby');
+    app.send(message);
+  }
 
   // renderRoom() {
+  // render room
+  // show the lobby or desired channel
+  // hide all the other channels when you enter your channel
+  // update new channels into the drop menu
   // }
 
 
 }
 
 
+// *** new obj message ***
+class Message {
+  constructor(username, text, roomname) {
+    this.username = username;
+    this.text = text;
+    this.roomname = roomname;
+  }
+}
+
 // create new object
 const app = new App();
 
 //call the init method every 30seconds
 setInterval(function() {
-  app.init();
-}, 30000);
+  app.fetch();
+}, 2500);
 
 
 
-
+  
+// *** JQuery ***
 $(document).ready(function() {
 
-$('button').click(function() {
-  $('#chats').empty();
-});  
+
+  // *** clear and refresh chat button ***
+  $('button').click(function() {
+    $('#chats').empty();
+  });  
+
+  // *** send message ***
+  $('form').submit(function(event) {
+    event.preventDefault();
+    app.renderMessages();  
+  });
+
 
 
 });
+
+
 
 
 
